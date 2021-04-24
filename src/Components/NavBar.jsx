71f8a915a002a9408634fa412311ae1ef925be20
Nelson Amador <rod6214@gmail.com>
2021-04-24 09:14:19 -0300

    Centred the input bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,9 +8,9 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-meli">
         <div className="container-fluid">
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <form className="d-flex">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
+                <form className="d-flex mx-auto">
+                    <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn-meli btn-outline-success" type="submit">Search</button>
                 </form>
             </div>
         </div>
