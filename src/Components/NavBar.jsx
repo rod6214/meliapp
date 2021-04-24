9683999d62562725daa42cc6c99473e06cf567ab
Nelson Amador <rod6214@gmail.com>
2021-04-24 13:42:21 -0300

    Added rounded styles

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,12 +5,12 @@ import './styles/navbar.scss'
 const NavBar = () => {
 
     return (
-    <nav className="navbar navbar-expand navbar-dark bg-meli">
+    <nav className="navbar navbar-expand navbar-dark bg-yelow-meli">
         <div className="container-fluid">
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <form className="d-flex mx-auto">
-                    <input className="form-control-meli" type="text" placeholder="Search" aria-label="Search" />
-                    <button className="btn-meli btn-outline-success" type="submit">
+                    <input className="form-control-meli rounded-start" type="text" placeholder="Nunca dejes de buscar" aria-label="Search" />
+                    <button className="btn-meli bg-grey-meli border-grey-meli rounded-end rounded-0" type="submit">
                         <i className="bi bi-search"></i>
                     </button>
                 </form>
