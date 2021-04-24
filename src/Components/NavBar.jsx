48193f92149560de9fb0ecf2d22b0a88ea945ff5
Nelson Amador <rod6214@gmail.com>
2021-04-24 10:53:31 -0300

    Package

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,11 +5,11 @@ import './styles/navbar.scss'
 const NavBar = () => {
 
     return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-meli">
+    <nav className="navbar navbar-expand navbar-dark bg-meli">
         <div className="container-fluid">
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <form className="d-flex mx-auto">
-                    <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                    <input className="form-control-meli" type="text" placeholder="Search" aria-label="Search" />
                     <button className="btn-meli btn-outline-success" type="submit">Search</button>
                 </form>
             </div>
