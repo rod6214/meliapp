8b11f511c9f9c5c392cbf8cdf17f2eac3858941e
Nelson Amador <rod6214@gmail.com>
2021-04-24 17:25:13 -0300

    Change page name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import TestPage from './Pages/TestPage';
+import TestPage from './Pages/ListPage';
 
 import './styles/base.scss'
 
