1ab6ca0fb11306e4c225c9811dce80d1984c086b
Nelson Amador <rod6214@gmail.com>
2021-04-25 18:07:55 -0300

    Implemented product component

--- src/Pages/DetailPage.jsx
+++ src/Pages/DetailPage.jsx
@@ -3,9 +3,7 @@ import Section from '../Components/Containers/Section'
 import Page from '../Components/Containers/Page';
 import Content from '../Components/Containers/Content';
 import Navigator from '../Components/Navs/Navigator';
-import Flex from '../Components/Containers/Flex';
-import Img from '../Components/Illustrators/Img';
-import Button from '../Components/Forms/Control/Button';
+import Product from '../Components/Containers/Product';
 
 import tagLinksMock from '../mocks/tagLinks.json'
 
@@ -18,25 +16,14 @@ const DetailPage = () => {
             </Content>
         </Section>
         <Section>
-            <Content padding={8} rounded={4} width={75} maxWidth="1100px" backgroundColor='white'>
-                <Flex alignment="justify-content-between">
-                        <Img width={680} src="http://http2.mlstatic.com/D_955205-MLA44925999950_022021-I.jpg" alt="detail-image" />
-                        <div className="p-0 m-0">
-                            <div className="pb-7"><span className="fs-10">Nuevo - 234 vendidos</span></div>
-                            <h1 className="fs-11 m-0 pb-8">Deco Reverse Sombrero Oxford</h1>
-                            <div className="pb-8">
-                                <meta itemProp="price" content="1980"/>
-                                <span className="fs-12">$ 1.980</span>
-                            </div>
-                            
-                            <Button className="btn btn-primary-meli rounded w-100 text-light fs-11" type="button">Comprar</Button>
-                        </div>
-                    </Flex>
-                <div>
-                    <h2 className="pb-8">Descripcion del producto</h2>
-                    <p className="w-75 text-grey-dark-meli">The Scarpe di Bianco footwear was founded by Bill White in 2009. Di Bianco offers classic handmade men's shoes with a modern twist. The combination of timeless models and details with contemporary colors and styling, results in decidedly current, yet, elegant models. The aim of the Scarpe di Bianco company is to offer men a custom shoe buying experience trough a multitude of models, lasts, soles, leathers, and color options.</p>
-                </div>
-            </Content>
+            <Product thumbnail="http://http2.mlstatic.com/D_955205-MLA44925999950_022021-I.jpg" 
+                     prodCondition="Nuevo"
+                     soldProducts="234"
+                     title="Deco Reverse Sombrero Oxford"
+                     currency="$"
+                     price="1.980"
+                     description_title="Descripcion del producto"
+                     description="The Scarpe di Bianco footwear was founded by Bill White in 2009. Di Bianco offers classic handmade men's shoes with a modern twist. The combination of timeless models and details with contemporary colors and styling, results in decidedly current, yet, elegant models. The aim of the Scarpe di Bianco company is to offer men a custom shoe buying experience trough a multitude of models, lasts, soles, leathers, and color options."/>
         </Section>
     </Page>
     )
