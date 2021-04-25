df216614814889ff108400ece906e65c4ac1b214
Nelson Amador <rod6214@gmail.com>
2021-04-25 19:40:56 -0300

    Added link into the logo image

--- src/Components/Navs/SearchBar.jsx
+++ src/Components/Navs/SearchBar.jsx
@@ -5,13 +5,16 @@ import Img from '../Illustrators/Img';
 import Icon from '../Illustrators/Icon';
 import Button from '../Forms/Control/Button';
 import Flex from '../Containers/Flex';
+import { Link } from "react-router-dom";
 
 const SearchBar = (props) => {
 
     return (
         <Form width={100} alignment="justify-content-center" flex>
             <Flex width={75} maxWidth={props.maxWidth} alignment="justify-content-center">
-                <Img me={6} src={props.src} alt="logo" width={60}/>
+                <Link className="me-6" to="/">
+                    <Img src={props.src} alt="logo" width={60}/>
+                </Link>
                 <Input />
                 <Button type="submit" search>
                     <Icon />
