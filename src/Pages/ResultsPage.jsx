3b1f908b00936978a96a1049c3fe7f24f9cbea6d
Nelson Amador <rod6214@gmail.com>
2021-04-27 01:08:18 -0300

    Documentado de la result page

--- src/Pages/ResultsPage.jsx
+++ src/Pages/ResultsPage.jsx
@@ -1,40 +1,30 @@
 import React from 'react-dom';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Section from '../Components/Containers/Section'
 import Page from '../Components/Containers/Page';
 import Content from '../Components/Containers/Content';
 import Card from '../Components/Containers/Card';
 import Navigator from '../Components/Navs/Navigator';
-import { useService, useSearch } from '../CustomHooks/useService';
+import { useSearch } from '../CustomHooks/useService';
 import { useQuery } from '../CustomHooks/useQuery';
 
 import tagLinksMock from '../mocks/tagLinks.json'
 
 const ResultsPage = () => {
-    // const [searchResult, setSearchResult] = useState([]);
+    /*
+     * Hook de busqueda limitado para un resultado de solo cuatro elementos
+     * basado en las especificaciones de los requerimientos del ejercicio
+     */
     const [searchResult, loaded, searchProducts] = useSearch(4);
-    // const [loaded, setLoaded] = useState(false);
-    // const { search } = useService().meliData;
+    // Recuperamos los datos de la query a traves de un hook
     const query = useQuery().get('search');
 
+    // Se usa effect para cargar los datos despues de renderizar la pagina
     useEffect(() => {
         searchProducts(query);
-        
-        console.log(searchResult)
-        // try {
-        //     const fetchProducts = async () => {
-        //         const matchedItems = await search(query, 4);
-        //         setSearchResult(matchedItems);
-        //         setLoaded(true);
-        //         console.log('r')
-        //         console.log(matchedItems)
-        //     }
-        //     fetchProducts();
-        // } catch (error) {
-        //     console.error(error);
-        // }
     }, [query, loaded]);
 
+    // Mostrar un contenedor vacion hasta que no se carguen los datos
     if (!loaded) return (<></>);
 
     return (
@@ -46,7 +36,7 @@ const ResultsPage = () => {
             </Section>
             <Section>
                 <Content rounded={4} width={75} maxWidth="1100px" backgroundColor='white'>
-                    {searchResult.map((item, index) => {
+                    {searchResult?.map((item, index) => {
                         
                         const hasDivider = (index < searchResult.length - 1);
 
